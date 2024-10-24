1ccdbafeb47f9ae2ba63fe1b353c09184b918e32
nithin.p@thinkpalm.com <nithin.p@thinkpalm.com>
2024-10-24 11:12:42 +0530

    task1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [flip, setFlip] = useState([]);
+  const [moves, setMoves] = useState(0);
+  const [match, setMatch] = useState(0);
+  const [finish, setFinish] = useState(false);
+  useEffect(() => {
+    const newCards = createCards();
+    setCards(shuffle(newCards));
+  }, []);
+  const shuffle = (s) => {
+    return s.sort(() => 0.5 - Math.random());
+  }
+
+  const handleFlip = (index) => {
+    if (flip?.length === 2) {
+      window.location.reload();
+    }
+    else {
+      setFlip([...flip, index]);
+      setMoves(moves + 1);
+    }
+    const card1 = flip[0];
+    const card2 = flip[1];
+    if (card1 === card2) {
+      setMatch(match + 1);
+      setFlip([]);
+      setFinish(true);
+    }
+  }
+  const createCards = () => {
+    const cardData = [
+      { id: 1, value: '🐶' },
+      { id: 2, value: '🐱' },
+      { id: 3, value: '🐭' },
+      { id: 4, value: '🐹' },
+      { id: 5, value: '🐰' },
+      { id: 6, value: '🦊' },
+      { id: 7, value: '🐻' },
+      { id: 8, value: '🐼' }
+    ];
+    const cards = cardData.map((card) =>
+      ({ ...card, flipped: false }));
+    return cards
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coding Challenge
-        </a>
+        <h2>
+          4x4 Game
+          <div>{finish ? 'Game over' : ''}</div>
+          {cards.map((card, index) => (
+            <div key={index}
+              style={{
+                padding: '10px',
+              }}
+              onClick={() => handleFlip(index)}
+            >
+              <div>{card.value}</div>
+              <div>{card.value}</div>
+
+            </div>
+          ))}
+        </h2>
       </header>
     </div>
   );
